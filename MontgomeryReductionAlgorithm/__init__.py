e52a0809a74cdc2fbefe5c8b6ccccad180d94582
Andreas Weber <Andreas.Willibald.Weber@gmail.com>
2024-06-28 16:00:05 +0200

    fix version

--- MontgomeryReductionAlgorithm/__init__.py
+++ MontgomeryReductionAlgorithm/__init__.py
@@ -2,7 +2,7 @@ from MontgomeryReductionAlgorithm.mmr import MMR
 from MontgomeryReductionAlgorithm.srsa import RSA
 
 
-__version__ = "0.0.2-alpha"
+__version__ = "0.0.3-alpha"
 __author__ = "Montgomery Reduction Algorithm"
 
 __all__ = [
